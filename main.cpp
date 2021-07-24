160ada68878192cb83ea37188ceae9f5c55dfcd7
Rahul <34953559+rahulk-code@users.noreply.github.com>
2021-07-24 09:59:28 +0530

    git check

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "everything.h"
 using namespace std;
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << "continue";
+    cout << "hellow";
 }
